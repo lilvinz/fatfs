be2ffe78d3f56b945621801a972ee0ba4a233ea0
vke <vincent.kessler@quantec-networks.de>
2015-05-03 22:51:44 +0200

    library.mk: add unicode.c to CSRC

--- library.mk
+++ library.mk
@@ -2,4 +2,5 @@
 
 FATFS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(FATFS_DIR)/src/ff.c)
+CSRC += $(wildcard $(FATFS_DIR)/src/option/unicode.c)
 EXTRAINCDIRS += $(FATFS_DIR)/src
