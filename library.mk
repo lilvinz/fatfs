f3032dc9014e7b8c29fea8ebf25524de4e86bd33
vke <vincent.kessler@quantec-networks.de>
2015-05-25 19:26:26 +0200

    library.mk: add HAS_FATFS define to preprocessor

--- library.mk
+++ library.mk
@@ -4,3 +4,5 @@ FATFS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CSRC += $(wildcard $(FATFS_DIR)/src/ff.c)
 CSRC += $(wildcard $(FATFS_DIR)/src/option/unicode.c)
 EXTRAINCDIRS += $(FATFS_DIR)/src
+
+CFLAGS += -DHAS_FATFS
